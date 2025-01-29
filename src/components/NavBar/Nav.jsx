35fafa35d1461716260588f72b21be5c7d03d54d
Lovish Duggal <lovishduggal11@gmail.com>
2025-01-29 16:57:35 +0530

    style: Add shadow to navbar for improved visual hierarchy

--- src/components/NavBar/Nav.jsx
+++ src/components/NavBar/Nav.jsx
@@ -11,7 +11,7 @@ function Nav() {
     document.querySelector('html').setAttribute('data-theme', theme);
   }, [theme]);
   return (
-    <div className="navbar bg-base-100 sticky top-0 w-full z-20">
+    <div className="navbar bg-base-100 sticky top-0 w-full z-20 shadow">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className=" btn btn-ghost md:hidden">
